259cc8a6221cec6ea7377d5d2db93374b492eec2
moskowitza <moskowitzaaron@mac.com>
2018-06-29 18:32:36 -0400

    lets see

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -5,14 +5,33 @@ module.exports = function (app, passport) {
     // GET routes to render our handlebar pages
     app.get('/signup', authController.signup);
     app.get('/signin', authController.signin);
+        //WE need a logout
+    app.get('/logout', authController.logout);
 
     // POST route to implement passport and sign up a user
     /**Since we need passport, we need to pass it to this method. 
      * We can import passport in this script OR pass it from server.js. NOTE WE TAKE IN APP, Passport as params */
     app.post('/signup', passport.authenticate('local-signup', {
-        successRedirect: '/dashboard',
+        successRedirect: '/dashboard', //GET path defined below
         failureRedirect: '/signup'
     }
     ));
-    app.get('/dashboard',authController.dashboard);
+    // Our successful redirect needs a get path. wow.
+
+
+   
+    // A function to see if we're logged in to protect the routes
+    //we pass it back to the dashboard get route
+    function isLoggedIn(req, res, next) {
+        if (req.isAuthenticated())
+            return next();
+        res.redirect('/signin');
+    }
+    app.get('/dashboard',isLoggedIn, authController.dashboard);
+
+    app.post('/signin', passport.authenticate('local-signin', {
+        successRedirect: '/dashboard',
+        failureRedirect: '/signin'
+    }
+));
 }
