7cef0217814d05591bfde019ceeb75475fb66279
moskowitza <moskowitzaaron@mac.com>
2018-06-29 15:43:13 -0400

    new authcontroller

--- app/controllers/authcontroller.js
+++ app/controllers/authcontroller.js
@@ -4,3 +4,6 @@ var exports = module.exports = {}
 exports.signup = function(req, res) {
     res.render('signup');
 }
+exports.signin = function(req, res) {
+    res.render('signin');
+}
--- app/routes/auth.js
+++ app/routes/auth.js
@@ -3,5 +3,5 @@ var authController = require('../controllers/authcontroller.js');
 //when we hit path /signup, auth controller calls a route, in this one case we just have signup
 module.exports = function (app) {
     app.get('/signup', authController.signup);
-    app.get('/signin', authController.signin);
+    // app.get('/signin', authController.signin);
 }
