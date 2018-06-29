6f74b669e92f5d573e9e1f9163e75eca8850054e
moskowitza <moskowitzaaron@mac.com>
2018-06-29 14:01:39 -0400

    throwing an error

---
+++ app/routes/auth.js
@@ -0,0 +1,6 @@
+var authController = require('../controllers/authcontroller.js');
+ 
+//when we hit path /signup, auth controller calls a route, in this one case we just have signup
+module.exports = function(app) {
+    app.get('/signup', authController.signup);
+}
--- server.js
+++ server.js
@@ -3,37 +3,46 @@
 */
 var express = require('express');
 var app = express();
-
-// import the dot-env module to handle environment variables.
-var env = require('dotenv').load();
-
 //middleware : import the passport module and the express-session, both of which we need to handle authentication.
 var passport   = require('passport');
 var session    = require('express-session');
 // Then, we import the body-parser module. This extracts the entire body part of an incoming request and exposes it in a format that is easier to work with. In this case, we will use the JSON format.
 var bodyParser = require('body-parser');
+// import the dot-env module to handle environment variables.
+var env = require('dotenv').load();
+
+//For bodyParser
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 //AFTER the bodyParser import line we initialize PASSPORT and the express session and passport session and add them both as middleware. We do this by adding these lines some spaces 
- 
 app.use(session({ secret: 'keyboard cat',resave: true, saveUninitialized:true})); // session secret
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 
-//Routes
-app.get('/', function(req, res) {
-    res.send('Welcome to Passport with Sequelize'); 
-});
-
+//For Handlebars
+var exphbs = require('express-handlebars');
+app.set('views', './app/views')
+app.engine('hbs', exphbs({
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
 //Models
 var models = require("./app/models"); 
+
+//Routes
+var authRoute = require('./app/routes/auth.js')(app);
+
+
 //Sync Database
 models.sequelize.sync({force:true}).then(function() {
   console.log('Nice! Database looks fine') 
 }).catch(function(err) {
     console.log(err, "Something went wrong with the Database Update!")
 });
+
+
+
 
 // Make app listen on port 5000. 
 app.listen(5000, function(err) {
